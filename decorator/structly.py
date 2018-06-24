9c7dc8b8f674fc0bb1893c66fc8999189a701c39
tonywywang <tony_wywang@163.com>
2018-06-24 11:24:43 -0400

    Another type check example by using class inheritance with Decriptor and Typed

--- decorator/structly.py
+++ decorator/structly.py
@@ -1,5 +1,37 @@
 from inspect import Parameter, Signature
 
+class Descriptor:
+	def __init__(self, name=None):
+		self.name = name
+
+	def __get__(self, instance):
+		print("Get", self.name)
+		return instance.__dict__[self.name]
+
+	def __set__(self, instance, value):
+		print("Set", self.name, value)
+		instance.__dict__[self.name] = value
+
+	def __del__(self, instance):
+		print("Delete", self.name)
+		del instance.__dict__[self.name]
+
+class Typed(Descriptor):
+	ty = object  # Expected type
+	def __set__(self, instance, value):
+		if not isinstance(value, self.ty):
+			raise TypeError("Expectd %s" % self.ty)
+		super().__set__(instance, value)
+
+class Integer(Typed):
+	ty = int
+
+class Float(Typed):
+	ty = float
+
+class String(Typed):
+	ty = str
+
 def make_signature(names):
 	return Signature(
 		Parameter(name, Parameter.POSITIONAL_OR_KEYWORD)
@@ -21,3 +53,10 @@ class Structure(metaclass=StructMeta):
 
 class Stock(Structure):
 	_fields = ['name', 'shares', 'price']
+	name = String('name')
+	shares = Integer('shares')
+	price = Float('price')
+
+# try import this file and inspect
+# print(inspect.signature(Stock))
+#
