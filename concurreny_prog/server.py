eb9546f67e1ac99cd122df5a01df719cdf1c02fd
Julie Zhu <juliemyzhu@gmail.com>
2018-08-25 17:08:25 -0400

    Add a micro server

---
+++ concurreny_prog/server.py
@@ -0,0 +1,29 @@
+# server.py
+# Fibonaci micro service
+# HOWTO test: using "nc localhost 25000"
+
+from socket import *
+from fib import fib
+
+def fib_server(address):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.bind(address)
+    sock.listen(5)
+    while True:
+        client, addr = sock.accept()
+        print("Connection", addr)
+        fib_handler(client)
+
+def fib_handler(client):
+    while True:
+        req = client.recv(100)
+        if not req:
+            break
+        n = int(req)
+        result = fib(n)
+        resp = str(result).encode('ascii') + b'\n'
+        client.send(resp)
+    print("Closed")
+
+fib_server(('',25000))
