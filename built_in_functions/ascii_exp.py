d331216ed1205e84171d8d5322147816f73c023f
tonywywang <tony_wywang@163.com>
2019-05-11 22:47:28 -0400

    Add built-in ascii function example

---
+++ built_in_functions/ascii_exp.py
@@ -0,0 +1,10 @@
+normalText = 'Python is interesting'
+print(ascii(normalText))
+
+otherText = 'Pythön is interesting'
+print(ascii(otherText))
+
+print('Pyth\xf6n is interesting')
+
+randomList = ['Python', 'Pythön', 5]
+print(ascii(randomList))
