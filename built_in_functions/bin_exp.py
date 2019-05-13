7977cb3c12f387f78625cac04affe6d2a068f9be
tonywywang <tony_wywang@163.com>
2019-05-12 22:45:53 -0400

    Add bin built-in function example

---
+++ built_in_functions/bin_exp.py
@@ -0,0 +1,4 @@
+print(bin(3))
+print(bin(-10))
+print(bin(0b110)) # bin function turn input into binary string with 0b
+# bin(0.2) bin('a') 'float' / 'str' object cannot be interpreted as an integer
