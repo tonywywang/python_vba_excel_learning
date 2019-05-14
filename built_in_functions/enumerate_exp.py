8636582c9101d7bd4160133802fe2f397c3183ee
tonywywang <tony_wywang@163.com>
2019-05-13 23:25:54 -0400

    Add enumerate built-in function example

---
+++ built_in_functions/enumerate_exp.py
@@ -0,0 +1,5 @@
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+k1 = list(enumerate(seasons))
+# [(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+type(k1[0]) # tuple
