b33e47db4c5cd8e6bf0fc0a2ceea9dd0bfd63998
tonywywang <tony_wywang@163.com>
2019-05-15 23:24:22 -0400

    Add format built-in function class example

--- built_in_functions/format_exp.py
+++ built_in_functions/format_exp.py
@@ -6,3 +6,11 @@ coord = {'latitude':'37.24N', 'longitude':'-115.81W'}
 print('Coodinators: {latitude} {longitude}'.format(**coord)) # Coodinators: 37.24N -115.81W
 c = 3-5j
 print("{0} has real {0.real} and imag {0.imag}".format(c)) # (3-5j) has real 3.0 and imag -5.0
+
+class Point:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+    def __str__(self):
+        return 'Point({self.x}, {self.y})'.format(self=self)
+c = Point(3, 4)
+print(c)   # Point(3, 4)
