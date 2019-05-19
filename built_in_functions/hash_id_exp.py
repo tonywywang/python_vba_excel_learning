72f14fd3ce1987a0fbc5adbf82285fa430c3749b
tonywywang <tony_wywang@163.com>
2019-05-19 07:09:07 -0400

    Add id hash value meaning comment

--- built_in_functions/hash_id_exp.py
+++ built_in_functions/hash_id_exp.py
@@ -5,3 +5,8 @@ print(hash(-1) == hash(-2))
 print(id(-1))
 print(id(-2))
 print(id(-1) == id(-2))
+
+# obj1 is obj2  means to compare their id value
+# == compares the objs' value
+# if an obj has a hash value, it means it can be used
+# as dictionary key
