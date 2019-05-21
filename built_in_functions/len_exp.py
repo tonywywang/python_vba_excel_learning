be2042256d81a3e55f1e12a58b7dd6711eaf41e8
tonywywang <tony_wywang@163.com>
2019-05-20 22:56:47 -0400

    Add len built-in function example

---
+++ built_in_functions/len_exp.py
@@ -0,0 +1,10 @@
+l1 = [1, 2, 3, 4]
+l2 = [1, 2, 3, 4]
+print(id(l1), id(l2))
+len(l1) # 4
+d1 = {'key1': 1, 'key2':2}
+len(d1) # 2
+g1 = (i for i in range(50))
+len(g1) # TypeError: object of type 'generator' has no len()
+t1 = (1, 2, 3)
+len(t1)
