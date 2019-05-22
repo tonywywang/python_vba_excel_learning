8a299a47c103dac78a9cefbecc2478961fa4357c
tonywywang <tony_wywang@163.com>
2019-05-21 22:29:16 -0400

    Add map built-in function turning iterator into list/set/tuple example

--- built_in_functions/map_exp.py
+++ built_in_functions/map_exp.py
@@ -13,3 +13,15 @@ my_str = map(to_upper_case, ['a', 'ab' ,'abc'])
 print_iter(my_str)
 my_list = map(lambda x: x**2, [1, 2, 3, 4])
 print_iter(my_list)
+
+map_iterator = map(to_upper_case, ['a', 'b', 'c'])
+my_list = list(map_iterator)
+print(my_list)
+
+map_iterator = map(to_upper_case, ['a', 'b', 'c'])
+my_set = set(map_iterator)
+print(my_set)
+
+map_iterator = map(to_upper_case, ['a', 'b', 'c'])
+my_tuple = tuple(map_iterator)
+print(my_tuple)
