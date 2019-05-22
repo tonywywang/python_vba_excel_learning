fb80c83aa2263ef6bc95a3dc5b71b02dae3e9bad
tonywywang <tony_wywang@163.com>
2019-05-21 22:33:23 -0400

    Add map built-in function example running result

--- built_in_functions/map_exp.py
+++ built_in_functions/map_exp.py
@@ -6,22 +6,22 @@ def print_iter(obj):
         print(x, end=' ')
     print("\n")
 my_str = map(to_upper_case, 'abc')
-print_iter(my_str)
+print_iter(my_str)  # A B C
 my_str = map(to_upper_case, ['a', 'b' ,'c'])
-print_iter(my_str)
+print_iter(my_str)  # A B C
 my_str = map(to_upper_case, ['a', 'ab' ,'abc'])
-print_iter(my_str)
+print_iter(my_str)  # A AB ABC
 my_list = map(lambda x: x**2, [1, 2, 3, 4])
-print_iter(my_list)
+print_iter(my_list)  # 1 4 9 16
 
 map_iterator = map(to_upper_case, ['a', 'b', 'c'])
 my_list = list(map_iterator)
-print(my_list)
+print(my_list) # [A, B, C]
 
 map_iterator = map(to_upper_case, ['a', 'b', 'c'])
 my_set = set(map_iterator)
-print(my_set)
+print(my_set) # {A, B, C}
 
 map_iterator = map(to_upper_case, ['a', 'b', 'c'])
 my_tuple = tuple(map_iterator)
-print(my_tuple)
+print(my_tuple) # (A, B, C)
