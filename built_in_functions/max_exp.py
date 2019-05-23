e313582428dc31ab20755d54cebdad3027ddea9b
tonywywang <tony_wywang@163.com>
2019-05-22 23:46:37 -0400

    Add max built-in function key in use to compare example

--- built_in_functions/max_exp.py
+++ built_in_functions/max_exp.py
@@ -3,3 +3,12 @@ print(max((2,4,-2,9)))  # 9
 print(max({1, 3, 5, -1})) # 5
 print(max({'c':1, 'ab':2, 'ac':-3})) # c
 print(max({'d':1, 'ab':2, 'dc':-3})) # dc
+
+def string_len(str):
+    return len(str)
+print(max('my','name','dictionary', key=string_len))
+
+x1 = [10, 20, 30]
+x2 = [5, 15, 40, 25]
+
+print(max(x1, x2, key=len))
