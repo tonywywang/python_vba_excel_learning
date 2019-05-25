537fab9059e484d57cfd11835c0fd12f7d4c8b81
tonywywang <tony_wywang@163.com>
2019-05-24 21:10:13 -0400

    Add open built-in function read file by line example

--- built_in_functions/open_exp.py
+++ built_in_functions/open_exp.py
@@ -9,3 +9,6 @@ with open("test.txt", 'w') as f:  # flush the file and add new content
     f.write('ccc')
 with open("test.txt", 'rb') as f:  # read mode
     print(f.read())         # b'eee\r\nccc'
+with open("test.txt", 'r') as f:  # read by line
+    for line in f:
+        print(line)
