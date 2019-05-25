0a2541a6ad81bd9e8d7b5885f16a2e5ec0f252cc
tonywywang <tony_wywang@163.com>
2019-05-24 21:00:04 -0400

    Add open built-in function binary mode example

--- built_in_functions/open_exp.py
+++ built_in_functions/open_exp.py
@@ -5,3 +5,7 @@ with open("test.txt", 'a') as f:  # append mode
     f.write('ddd')
 with open("test.txt", 'w') as f:  # flush the file and add new content
     f.write('eee')
+    f.write('\n')
+    f.write('ccc')
+with open("test.txt", 'rb') as f:  # read mode
+    print(f.read())         # b'eee\r\nccc'
