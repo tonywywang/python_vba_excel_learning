1ad97f75558f5781d2d3c86a1824a64fe8402114
tonywywang <tony_wywang@163.com>
2019-05-27 22:14:44 -0400

    Add __str__ and __repr__ method difference example

--- built_in_functions/repr_exp.py
+++ built_in_functions/repr_exp.py
@@ -6,3 +6,15 @@ class A:
         return "class A __repr__ method"
 a1 = A()
 print(a1)  # class A __repr__ method
+
+class A1:
+    def __init__(self):
+        self._age = 10
+        self._height = 20
+    def __repr__(self):
+        return "class A __repr__ method"
+    def __str__(self):
+        return "Age:" + str(self._age) + " Height:" + str(self._height) 
+a2 = A1()
+print(a2) # Age:10 Height:20
+repr(a2)  # class A __repr__ method
