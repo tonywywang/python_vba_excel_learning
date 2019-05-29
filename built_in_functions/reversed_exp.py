38f44dd0dc5c802db0c839c7326568fc205a9ea1
tonywywang <tony_wywang@163.com>
2019-05-28 22:00:34 -0400

    Indicate reversed function only be used for iterator but not for generator

--- built_in_functions/reversed_exp.py
+++ built_in_functions/reversed_exp.py
@@ -5,3 +5,14 @@ for num in reversed(l1):
     print(num)
 for i in reversed(range(10)):
     print(i)
+
+# reversed only can reverse iterator
+def fib(num):
+    a, b = 1, 1
+    while num >= 0:
+        yield a
+        a, b = b, a + b
+        num = num - 1
+        
+for num in reversed(fib(5)): # Error: 'generator' object is not reversible
+    print(num) 
