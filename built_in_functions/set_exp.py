ca170f6876aa03b76bca6291dffc0b469991d994
tonywywang <tony_wywang@163.com>
2019-05-29 22:36:27 -0400

    Add set built-in class add/update method example

--- built_in_functions/set_exp.py
+++ built_in_functions/set_exp.py
@@ -1,3 +1,11 @@
 l1 = [1, 2, 3, 2, 4, 2, 4, 5, 3]
 s1 = set(l1)
 print(s1)
+
+s1 = set()
+s1.add(1)
+# s1.add({1, 2}) #TypeError: unhashable type: 'set'
+s1.update({1, 2})
+s1.add(2)
+s1.add(3)
+print(s1)
