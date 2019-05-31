4ec6237ef5dffb8567f502d3abea5390d4c891a6
tonywywang <tony_wywang@163.com>
2019-05-30 23:46:32 -0400

    Add set built-in class discard method example

--- built_in_functions/set_exp.py
+++ built_in_functions/set_exp.py
@@ -9,3 +9,13 @@ s1.update({1, 2})
 s1.add(2)
 s1.add(3)
 print(s1)
+
+theSet = {1,2,3,4,5,6}
+
+#remove 3 using discard() function
+theSet.discard(3)
+print(theSet)
+
+#call discard() function again to remove 3
+theSet.discard(3) #This won't raise any exception
+print(theSet)
