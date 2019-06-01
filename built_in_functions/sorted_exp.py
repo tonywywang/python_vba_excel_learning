40d422e238e4be0ff43b52c55021a1cf0482bda7
tonywywang <tony_wywang@163.com>
2019-05-31 22:37:14 -0400

    Add sorted built-in function symbol example

--- built_in_functions/sorted_exp.py
+++ built_in_functions/sorted_exp.py
@@ -2,3 +2,6 @@ strlist = ['s', 'q', 'I', 'g', 'w', 'Z', 't']
 print(sorted(strlist))                                    # ['I', 'Z', 'g', 'q', 's', 't', 'w']
 print(sorted(strlist, key = str.lower))                   # ['g', 'I', 'q', 's', 't', 'w', 'Z']
 print(sorted(strlist, key = str.lower, reverse = True))   # ['Z', 'w', 't', 's', 'q', 'I', 'g']
+
+strlist = ['s', 'q', '.', 'I', 'g', '(', '我', 'w', 'Z', 't', ' ', ',']  # [' ', '(', ',', '.', 'I', 'Z', 'g', 'q', 's', 't', 'w', '我']
+sorted(strlist)
