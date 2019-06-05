7278838c3122903e546c0241c4e9dcc4b26740f2
tonywywang <tony_wywang@163.com>
2019-06-05 17:36:02 -0400

    Add string library constants example

---
+++ library_example/string_lib_exp.py
@@ -0,0 +1,11 @@
+import string
+
+string.digits             # '0123456789'
+string.ascii_letters      # 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+string.ascii_lowercase    # 'abcdefghijklmnopqrstuvwxyz'
+string.ascii_uppercase    # 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+string.hexdigits          # '0123456789abcdefABCDEF'
+string.octdigits          # '01234567'
+string.punctuation        # '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+string.printable          # '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
+string.whitespace         # ' \t\n\r\x0b\x0c'
