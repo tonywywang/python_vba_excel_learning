55349a345762d8f7ce6ac735dcc42ba94c1b1f2e
tonywywang <tony_wywang@163.com>
2019-06-05 17:46:56 -0400

    Add string library format number hex/dec/oct example

--- library_example/string_lib_exp.py
+++ library_example/string_lib_exp.py
@@ -15,3 +15,16 @@ string.whitespace         # ' \t\n\r\x0b\x0c'
 '{2}, {1}, {0}'.format('a', 'b', 'c')  # 'c, b, a'
 '{2}, {1}, {0}'.format(*'abc')      # unpacking argument sequence 'c, b, a'
 '{0}{1}{0}'.format('abra', 'cad')   # arguments' indices can be repeated 'abracadabra'
+
+'Coordinates: {latitude}, {longitude}'.format(latitude='37.24N', longitude='-115.81W') #'Coordinates: 37.24N, -115.81W'
+coord = {'latitude': '37.24N', 'longitude': '-115.81W'}
+'Coordinates: {latitude}, {longitude}'.format(**coord)      #'Coordinates: 37.24N, -115.81W'
+
+'{:+f}; {:+f}'.format(3.14, -3.14)  # show it always #'+3.140000; -3.140000'
+'{: f}; {: f}'.format(3.14, -3.14)  # show a space for positive numbers #' 3.140000; -3.140000'
+'{:-f}; {:-f}'.format(3.14, -3.14)  # show only the minus -- same as '{:f}; {:f}' #'3.140000; -3.140000'
+
+# format also supports binary numbers
+"int: {0:d};  hex: {0:x};  oct: {0:o};  bin: {0:b}".format(42) #'int: 42;  hex: 2a;  oct: 52;  bin: 101010'
+# with 0x, 0o, or 0b as prefix:
+"int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}".format(42) #'int: 42;  hex: 0x2a;  oct: 0o52;  bin: 0b101010'
