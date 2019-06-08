c30e763dfdb2d10d1e7715b2784206a141ee8e8c
tonywywang <tony_wywang@163.com>
2019-06-08 10:08:03 -0400

    Add list slice interval example

--- library_example/list_exp.py
+++ library_example/list_exp.py
@@ -2,3 +2,6 @@ l1 = [1, 2, 3, 4]
 #del l1[1:3]
 l1[1:3] = [5, 5]   # slice need to be assigned with iterable obj [1, 5, 5, 4]
 del l1[1:3]        # [1, 4]
+l2 = [1, 2, 3, 4, 6, 7, 8, 9]
+l2[1:5:2]  # [2, 4]        1: start 5: end 2: interval
+l2[1:5:1]  # [2, 3, 4, 6]  1: start 5: end 1: interval
