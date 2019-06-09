16bbda23516bd96493c8b3b649eadbd470ca2dd4
tonywywang <tony_wywang@163.com>
2019-06-09 19:32:36 -0400

    Add list built-in method append/extend example

--- library_example/list_exp.py
+++ library_example/list_exp.py
@@ -5,3 +5,7 @@ del l1[1:3]        # [1, 4]
 l2 = [1, 2, 3, 4, 6, 7, 8, 9]
 l2[1:5:2]  # [2, 4]        1: start 5: end 2: interval
 l2[1:5:1]  # [2, 3, 4, 6]  1: start 5: end 1: interval
+
+l1.append(10)
+l1.extend([11, 12, 23])  # extend argument must be iterable object
+l1.extend(range(5))
