9ff4877c74af8b74a2ff59a5526f11d1d150e63f
tonywywang <tony_wywang@163.com>
2019-06-10 20:38:27 -0400

    Add re $ symbol MULTILINE flag example

--- library_example/re_exp.py
+++ library_example/re_exp.py
@@ -7,3 +7,7 @@ g = re.search('.', '\n', flags=re.DOTALL)  # with flag DOTALL, '.' (Dot.) can ma
 g = re.search('^B', 'A: line1\nB: line2\nC: line3')         # no match
 g = re.search('^B', 'A: line1\nB: line2\nC: line3', flags=re.MULTILINE) # match line 2 start 'B'
 g.group(0)  
+ 
+g = re.search('2$', 'A: line1\nB: line2\nC: line3')         # no match
+g = re.search('2$', 'A: line1\nB: line2\nC: line3', flags=re.MULTILINE) # match line 2 end with '2'
+g.group(0)
