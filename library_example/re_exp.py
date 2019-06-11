c68abef2cca13239563ad3544c0f45835e2ce188
tonywywang <tony_wywang@163.com>
2019-06-10 20:33:06 -0400

    Add re .(Dot) match symbol example

---
+++ library_example/re_exp.py
@@ -0,0 +1,5 @@
+import re
+
+g = re.search('.', 'abcdef') # '.' (Dot.) In the default mode, this matches any character except a newline. Result 'a'
+g = re.search('.', '\n')     # newline symbol, '.' (Dot.) can not match
+g = re.search('.', '\n', flags=re.DOTALL)  # with flag DOTALL, '.' (Dot.) can match newline symbol
