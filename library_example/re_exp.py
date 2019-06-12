4d23fa54c1f72cff6b88ced90f1a6877d0d76b4a
tonywywang <tony_wywang@163.com>
2019-06-11 22:43:14 -0400

    Add re * repetition example

--- library_example/re_exp.py
+++ library_example/re_exp.py
@@ -11,3 +11,15 @@ g.group(0)
 g = re.search('2$', 'A: line1\nB: line2\nC: line3')         # no match
 g = re.search('2$', 'A: line1\nB: line2\nC: line3', flags=re.MULTILINE) # match line 2 end with '2'
 g.group(0)
+
+rule = re.compile('a*')         # repetition of a, 'a', 'aa', 'aaa', 'aa...' matches
+g = rule.match('abb a ab')
+g.group(0)  # a
+
+rule = re.compile('ab*')         # repetition of b, 'ab', 'abb', 'abbb', 'abbb...' matches
+g = rule.match('abc a ab')
+g.group(0)  # ab
+
+rule = re.compile('ab*')         # repetition of a, 'a', 'aa', 'aaa', 'aa...' matches
+g = rule.match('cdeab')
+#g.group(0)  # no match None
