654038febf0088aebfcd4093486795be5c540450
tonywywang <tony_wywang@163.com>
2019-06-19 22:05:56 -0400

    Add \n and \n+ difference \n+ match more than 1

--- library_example/re_exp.py
+++ library_example/re_exp.py
@@ -145,3 +145,27 @@ if __name__=="__main__":
 print(re.escape('python.exe'))
 legal_chars = string.ascii_lowercase + string.digits + "!#$%&'*+-.^_`"
 print('[%s]+' % re.escape(legal_chars))
+
+text = """Ross McFluff: 834.345.1254 155 Elm Street
+
+Ronald Heathmore: 892.345.3428 436 Finley Avenue
+Frank Burger: 925.541.7625 662 South Dogwood Way
+
+
+Heather Albrecht: 548.326.4584 919 Park Place"""
+entries = re.split(r'\n', text)
+print(entries)
+['Ross McFluff: 834.345.1254 155 Elm Street',
+ '',
+ 'Ronald Heathmore: 892.345.3428 436 Finley Avenue',
+ 'Frank Burger: 925.541.7625 662 South Dogwood Way',
+ '',
+ '',
+ 'Heather Albrecht: 548.326.4584 919 Park Place']
+
+entries = re.split(r'\n+', text)
+print(entries)
+['Ross McFluff: 834.345.1254 155 Elm Street',
+ 'Ronald Heathmore: 892.345.3428 436 Finley Avenue',
+ 'Frank Burger: 925.541.7625 662 South Dogwood Way',
+ 'Heather Albrecht: 548.326.4584 919 Park Places]
