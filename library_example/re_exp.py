81d58fdf2a8b3b3f2939c08a4e30447480e4d116
tonywywang <tony_wywang@163.com>
2019-06-19 22:19:17 -0400

    Add split maxsplit entry times example

--- library_example/re_exp.py
+++ library_example/re_exp.py
@@ -168,4 +168,28 @@ print(entries)
 ['Ross McFluff: 834.345.1254 155 Elm Street',
  'Ronald Heathmore: 892.345.3428 436 Finley Avenue',
  'Frank Burger: 925.541.7625 662 South Dogwood Way',
- 'Heather Albrecht: 548.326.4584 919 Park Places]
+ 'Heather Albrecht: 548.326.4584 919 Park Places']
+
+ text = """Ross McFluff, 834.345.1254 155 Elm Street
+
+Ronald Heathmore: 892.345.3428 436 Finley Avenue
+Frank Burger: 925.541.7625 662 South Dogwood Way
+
+
+Heather Albrecht: 548.326.4584 919 Park Place"""
+entries = re.split(r'\n+', text)
+print(entries)
+['Ross McFluff: 834.345.1254 155 Elm Street',
+ 'Ronald Heathmore: 892.345.3428 436 Finley Avenue',
+ 'Frank Burger: 925.541.7625 662 South Dogwood Way',
+ 'Heather Albrecht: 548.326.4584 919 Park Places']
+[re.split("[:,]? ", entry, 3) for entry in entries]
+[['Ross', 'McFluff,', '834.345.1254', '155 Elm Street'],
+ ['Ronald', 'Heathmore', '892.345.3428', '436 Finley Avenue'],
+ ['Frank', 'Burger', '925.541.7625', '662 South Dogwood Way'],
+ ['Heather', 'Albrecht', '548.326.4584', '919 Park Place']]
+[re.split("[:,]? ", entry, 4) for entry in entries]
+[['Ross', 'McFluff', '834.345.1254', '155', 'Elm Street'],
+ ['Ronald', 'Heathmore', '892.345.3428', '436', 'Finley Avenue'],
+ ['Frank', 'Burger', '925.541.7625', '662', 'South Dogwood Way'],
+ ['Heather', 'Albrecht', '548.326.4584', '919', 'Park Place']]
