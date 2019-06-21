411b4455e4754a13abdac21014bfb6c7e2fd7a34
tonywywang <tony_wywang@163.com>
2019-06-20 22:55:49 -0400

    Add difflib SequenceMatcher how to deal with symbol example

--- library_example/difflib_exp.py
+++ library_example/difflib_exp.py
@@ -4,3 +4,13 @@ s1 =  [1, 2, 3, 5, 6, 4]
 s2 =  [2, 3, 5, 4, 6, 1]
 matched = difflib.SequenceMatcher(None, s1, s2)
 print(matched.find_longest_match(0, 6, 0, 6))  # the longest match in S1 starts at index 1, in S2 starts at index 0 length 3 [2, 3, 5]
+
+s1 = ' abcd'
+s2 = 'abcd abcd'
+matcher = difflib.SequenceMatcher(None, s1, s2)
+print(matcher.find_longest_match(0, 5, 0, 9))  # Match(a=0, b=4, size=5)
+
+s1 = ' abcd'
+s2 = 'abcd abcd'
+matcher = difflib.SequenceMatcher(lambda x:x in ' ', s1, s2)
+print(matcher.find_longest_match(0, 5, 0, 9))  # Match(a=1, b=0, size=4)
