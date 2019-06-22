8b181805c4e23564a0296e602631e7c947b00a36
tonywywang <tony_wywang@163.com>
2019-06-21 23:00:33 -0400

    Add get_grouped_opcodes and match ratio example

--- library_example/difflib_exp.py
+++ library_example/difflib_exp.py
@@ -19,3 +19,34 @@ f = lambda x:x in ' '
 f('')   # True
 f(' ')  # True
 f('1')  # False
+
+s1 = ' abcd 123 321'
+s2 = 'abcd abcd 123321'
+print('s1 = ', s1)
+print('s2 = ', s2)
+print('s1 == s2', s1 == s2)
+print('')
+matcher = difflib.SequenceMatcher(None, s1, s2)
+for i in matcher.get_grouped_opcodes(1):
+    print(i)
+    print(':')
+print(matcher)
+print('ratio():', matcher.ratio())
+print('quick_ratio():', matcher.quick_ratio())
+print('real_quick_ratio():', matcher.real_quick_ratio())
+'''
+s1 =   abcd 123 321
+s2 =  abcd abcd 123321
+s1 == s2 False
+
+[('delete', 0, 1, 0, 0), ('equal', 1, 2, 0, 1)]
+:
+[('equal', 4, 5, 3, 4), ('replace', 5, 6, 4, 10), ('equal', 6, 7, 10, 11)]
+:
+[('equal', 8, 9, 12, 13), ('delete', 9, 10, 13, 13), ('equal', 10, 11, 13, 14)]
+:
+<difflib.SequenceMatcher object at 0x000001C3B1DFE780>
+ratio(): 0.6896551724137931
+quick_ratio(): 0.6896551724137931
+real_quick_ratio(): 0.896551724137931
+'''
