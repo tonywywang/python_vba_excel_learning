df3250ae8ed480216f935d9a74b48aa8fcf6420e
tonywywang <tony_wywang@163.com>
2019-06-24 21:14:52 -0400

    Add textwrap fill example

--- library_example/textwrap_lib_exp.py
+++ library_example/textwrap_lib_exp.py
@@ -7,4 +7,18 @@ text1 = '''1. aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 print(textwrap.wrap(text1, width=10))
 # ['1. aaaaaaa', 'aaaaaaaaaa', 'aaaaaaaaaa', 'aa 2. bbbb', 'bbbbbbbbbb', 'bbbbbbbbbb', 'b 3. ccccc', 'cccccccccc', 'cccccccccc']
 print(textwrap.wrap(text1, width=70))
-# ['1. aaaaaaaaaaaaaaaaaaaaaaaaaaaaa 2. bbbbbbbbbbbbbbbbbbbbbbbbb 3.', 'ccccccccccccccccccccccccc']
+# ['1. aaaaaaaaaaaaaaaaaaaaaaaaaaaaa 2. bbbbbbbbbbbbbbbbbbbbbbbbb 3.', 'ccccccccccccccccccccccccc
+
+print(textwrap.fill(text1, width=10))
+'''
+1. aaaaaaa
+aaaaaaaaaa
+aaaaaaaaaa
+aa 2. bbbb
+bbbbbbbbbb
+bbbbbbbbbb
+b 3. ccccc
+cccccccccc
+cccccccccc
+'''
+# fill function works same as "\n".join(wrap(text, ...))
