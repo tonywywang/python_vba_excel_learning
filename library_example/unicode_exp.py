67e884b129aa4917f9db82aa4265c68d7975b276
tonywywang <tony_wywang@163.com>
2019-06-28 19:06:14 -0400

    Add unicode name example

--- library_example/unicode_exp.py
+++ library_example/unicode_exp.py
@@ -5,3 +5,21 @@ unicodedata.lookup('HYPHEN')
 # '‐'
 unicodedata.lookup('QUESTION MARK')
 # '?'
+unicodedata.name('.')
+# 'FULL STOP'
+unicodedata.name(',')
+# 'COMMA'
+unicodedata.name(':')
+# 'COLON'
+unicodedata.name(';')
+# 'SEMICOLON'
+unicodedata.name('"')
+# 'QUOTATION MARK'
+unicodedata.name('\'')
+# 'APOSTROPHE'
+unicodedata.name('\'')
+# 'REVERSE SOLIDUS'
+unicodedata.name('/')
+# 'SOLIDUS'
+unicodedata.name('王')
+# 'CJK UNIFIED IDEOGRAPH-738B'
