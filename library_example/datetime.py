62cbe00b0fb878e416ab3843ea53e6d4d9169ffc
tonywywang <tony_wywang@163.com>
2019-07-08 21:55:20 -0400

    Add datetime date example

--- library_example/datetime.py
+++ library_example/datetime.py
@@ -16,3 +16,13 @@ print(-t4)
 year = datetime.timedelta(days=365)   # 365 days
 another_year = datetime.timedelta(weeks=40, days=84, hours=23, minutes=50, seconds=600) # another 365 days
 print(another_year.total_seconds())   # 31536000.0
+
+d1 = datetime.date(2019, 7, 8)
+print(d1)
+# 2019-07-08
+
+#d1 = datetime.date(2019, 13, 8)
+# ValueError                                Traceback (most recent call last)
+#<ipython-input-8-a38be9935a92> in <module>()
+#----> 1 d1 = datetime.date(2019, 13, 8)
+# ValueError: month must be in 1..12
