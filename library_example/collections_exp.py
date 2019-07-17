e7ee7b7b42d34d6921f672a6c52256755069b42b
tonywywang <tony_wywang@163.com>
2019-07-17 19:42:57 -0400

    ChainMap only write or update the first mapping in the chain

--- library_example/collections_exp.py
+++ library_example/collections_exp.py
@@ -22,5 +22,11 @@ f = d.new_child()
 print(f)              # ChainMap({}, {}, {})
 print(e.maps[0])      # {}
 d['x'] = 1
-print(d)              # ChainMap({'x': 1}, {})
+d['y'] = 2            # ChainMap only write or update the first mapping in the chain
+print(d)              # ChainMap({'x': 1, 'y': 2}, {})
 print(f)              # ChainMap({}, {'x': 1}, {})
+
+d = ChainMap({'zebra': 'black'}, {'elephant': 'blue'}, {'lion': 'yellow'})
+d['lion'] = 'orange'
+print(d)
+# ChainMap({'zebra': 'black', 'lion': 'orange'}, {'elephant': 'blue'}, {'lion': 'yellow'})
