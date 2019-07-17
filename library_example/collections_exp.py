df053d541dacea0901677d7cb93437137b1c507b
tonywywang <tony_wywang@163.com>
2019-07-16 20:41:45 -0400

    ChainMap child and parent example

--- library_example/collections_exp.py
+++ library_example/collections_exp.py
@@ -11,3 +11,16 @@ print(list(combined))
 pylookup = ChainMap(locals(), globals(), vars(builtins))
 print(list(pylookup))
 print(pylookup['__name__'])  #__main__
+
+c = ChainMap()
+print(c)              # ChainMap({})
+d = c.new_child()
+print(d)              # ChainMap({}, {})
+e = c.new_child()
+print(e)              # ChainMap({}, {})
+f = d.new_child()
+print(f)              # ChainMap({}, {}, {})
+print(e.maps[0])      # {}
+d['x'] = 1
+print(d)              # ChainMap({'x': 1}, {})
+print(f)              # ChainMap({}, {'x': 1}, {})
