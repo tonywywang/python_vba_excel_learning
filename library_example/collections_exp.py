10861e1a915eac1df45dd02773830809dbba8c0d
tonywywang <tony_wywang@163.com>
2019-07-18 19:28:13 -0400

    Add collections counter example

--- library_example/collections_exp.py
+++ library_example/collections_exp.py
@@ -51,3 +51,9 @@ f['snake'] = 'red'
 del f['elephant']
 print(f)
 # DeepChainMap({'zebra': 'black', 'snake': 'red'}, {}, {'lion': 'orange'})
+
+cnt = Counter()
+for word in ['red', 'blue', 'red', 'green', 'blue', 'blue']:
+    cnt[word] += 1
+print(cnt)
+# Counter({'blue': 3, 'red': 2, 'green': 1})
