68705e4868045cca0ec36bfe0b12a34486c1c140
tonywywang <tony_wywang@163.com>
2019-07-18 19:31:31 -0400

    Add collections counter most_common example

--- library_example/collections_exp.py
+++ library_example/collections_exp.py
@@ -1,5 +1,7 @@
 from collections import *
 import builtins
+import re
+
 baseline = {'music': 'bach', 'art': 'rembrandt'}
 adjustments = {'art': 'van gogh', 'opera': 'carmen'}
 print(list(ChainMap(adjustments, baseline)))
@@ -57,3 +59,18 @@ for word in ['red', 'blue', 'red', 'green', 'blue', 'blue']:
     cnt[word] += 1
 print(cnt)
 # Counter({'blue': 3, 'red': 2, 'green': 1})
+
+words = re.findall(r'\w+', open('IPQ40xx-sflash-devreg.patch').read().lower())
+Counter(words).most_common(10)
+'''
+[('security', 104),
+ ('0', 95),
+ ('define', 91),
+ ('p_ctx', 75),
+ ('if', 62),
+ ('inc', 62),
+ ('flash', 61),
+ ('ubnt', 52),
+ ('return', 51),
+ ('target', 46)]
+'''
