f87df7dd951d2cb25c3717c011fc3e21c8f2da27
tonywywang <tony_wywang@163.com>
2019-08-03 21:32:14 -0400

    Add array type code

--- library_example/array_exp.py
+++ library_example/array_exp.py
@@ -1,3 +1,4 @@
 from array import *
 
-a1 = array('d', [1.0, 2.3, 3.14])
+a1 = array('d', [1.0, 2.3, 3.14]) # class array.array(typecode[, initializer])
+print(a1.typecode) # d (typecode 'd' double type)
