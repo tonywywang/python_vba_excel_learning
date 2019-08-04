a6ef711becdc8e31073138d5c97fc080f34b686c
tonywywang <tony_wywang@163.com>
2019-08-03 21:36:41 -0400

    Add array append method

--- library_example/array_exp.py
+++ library_example/array_exp.py
@@ -3,3 +3,4 @@ from array import *
 a1 = array('d', [1.0, 2.3, 3.14]) # class array.array(typecode[, initializer])
 print(a1.typecode) # d (typecode 'd' double type)
 print(a1.itemsize) # 8 The length in bytes of one array item in the internal representation.
+a1.append(-4.59) # like list to append value
