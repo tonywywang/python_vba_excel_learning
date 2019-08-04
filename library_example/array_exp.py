eb2f65e04f07e8b77b3d10dcc6754e2fd73bd2a4
tonywywang <tony_wywang@163.com>
2019-08-03 21:33:49 -0400

    Add array item size ie.type size

--- library_example/array_exp.py
+++ library_example/array_exp.py
@@ -2,3 +2,4 @@ from array import *
 
 a1 = array('d', [1.0, 2.3, 3.14]) # class array.array(typecode[, initializer])
 print(a1.typecode) # d (typecode 'd' double type)
+print(a1.itemsize) # 8 The length in bytes of one array item in the internal representation.
