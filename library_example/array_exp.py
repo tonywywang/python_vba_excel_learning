b1fb13ebb73f65780c0fa70cdad3e20e45cbd4a8
tonywywang <tony_wywang@163.com>
2019-08-03 21:38:13 -0400

    Add array byteswap method for litte endian to big endian

--- library_example/array_exp.py
+++ library_example/array_exp.py
@@ -4,3 +4,9 @@ a1 = array('d', [1.0, 2.3, 3.14]) # class array.array(typecode[, initializer])
 print(a1.typecode) # d (typecode 'd' double type)
 print(a1.itemsize) # 8 The length in bytes of one array item in the internal representation.
 a1.append(-4.59) # like list to append value
+print(a1) # array('d', [1.0, 2.3, 3.14, -4.59])
+# This is only supported for values which are 1, 2, 4, or 8 bytes in size; 
+# for other types of values, RuntimeError is raised. It is useful when reading
+# data from a file written on a machine with a different byte order.
+a1.byteswap() 
+print(a1) # array('d', [3.03865e-319, 1.9035985662475526e+185, 7.9824696849641e-157, 7.387368604390227e+137])
