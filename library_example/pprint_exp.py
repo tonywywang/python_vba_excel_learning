d445ee9beb7426f58587972813e9ba1566bc11c4
tonywywang <tony_wywang@163.com>
2019-08-06 22:21:35 -0400

    Add pprint example

---
+++ library_example/pprint_exp.py
@@ -0,0 +1,10 @@
+import pprint
+
+stuff = ['spam', 'eggs', 'lumberjack', 'knights', 'ni']
+pp = pprint.PrettyPrinter(width=1)
+pp.pprint(stuff)
+# ['spam',
+#  'eggs',
+#  'lumberjack',
+#  'knights',
+#  'ni']
