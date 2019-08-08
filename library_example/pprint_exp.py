b9a872a39348bc732b5c0cd9c20efb41f4c7b197
tonywywang <tony_wywang@163.com>
2019-08-07 22:05:55 -0400

    Add pprint pretty print example

--- library_example/pprint_exp.py
+++ library_example/pprint_exp.py
@@ -16,3 +16,30 @@ pp.pprint(stuff)
 #     'lumberjack',
 #     'knights',
 #     'ni']
+
+import json
+import pprint
+from urllib.request import urlopen
+
+with urlopen('https://pypi.org/pypi/sampleproject/json') as resp:
+    project_info = json.load(resp)['info']
+
+pprint.pprint(project_info)
+'''
+{'author': 'The Python Packaging Authority',
+ 'author_email': 'pypa-dev@googlegroups.com',
+ 'bugtrack_url': None,
+ 'classifiers': ['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Topic :: Software Development :: Build Tools'],
+ 'description': '# A sample Python project\n'
+ ...
+'''
