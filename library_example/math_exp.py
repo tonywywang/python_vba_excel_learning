4add734db8a1f3a0ef405d64c03f70c4c2f2b4c3
tonywywang <tony_wywang@163.com>
2019-08-09 22:06:59 -0400

    Add Math member method example

---
+++ library_example/math_exp.py
@@ -0,0 +1,12 @@
+from math import *
+​
+print(ceil(3.000001))
+# 4
+print(fabs(-3.000001))
+# 3.000001 absolute value
+print(copysign(-3.0, -0.0))
+print(copysign(-3.0, 0.0))
+# -3.0  copysign(x, y) return x absolute value and with sign y
+# 3.0
+print(floor(3.00001))
+3
