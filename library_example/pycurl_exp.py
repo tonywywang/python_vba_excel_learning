2b94701dd316f2b3c6bca375bed9942508fb9e0d
tonywywang <tony_wywang@163.com>
2019-08-11 16:47:48 -0400

    Add pycurl http example

---
+++ library_example/pycurl_exp.py
@@ -0,0 +1,15 @@
+import pycurl
+import sys
+#from StringIO import StringIO
+
+with open("curl_test.txt", 'wb') as f:
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://pycurl.io/')
+    c.setopt(c.WRITEDATA, f)
+    c.perform()
+    c.close()
+
+with open("curl_test.txt", 'rb') as f:
+    line = f.readlines()
+    for elem in line:
+        print(elem)
