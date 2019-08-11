6153e2a0017bb7fcfba7ea914e15caec528bc447
tonywywang <tony_wywang@163.com>
2019-08-11 17:08:31 -0400

    Add pycurl https example

--- library_example/pycurl_exp.py
+++ library_example/pycurl_exp.py
@@ -13,3 +13,29 @@ with open("curl_test.txt", 'rb') as f:
     line = f.readlines()
     for elem in line:
         print(elem)
+
+import certifi
+PY3 = sys.version_info[0] > 2
+
+
+class Test:
+    def __init__(self):
+        self.contents = ''
+        if PY3:
+            self.contents = self.contents.encode('ascii')
+
+    def body_callback(self, buf):
+        self.contents = self.contents + buf
+
+
+sys.stderr.write("Testing %s\n" % pycurl.version)
+
+t = Test()
+c = pycurl.Curl()
+c.setopt(pycurl.CAINFO, certifi.where())
+c.setopt(c.URL, 'https://curl.haxx.se/dev/')
+c.setopt(c.WRITEFUNCTION, t.body_callback)
+c.perform()
+c.close()
+
+print(t.contents)
