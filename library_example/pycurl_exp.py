71a74bb0604fa20eb19b81f3410f9ecbfffff1bf
tonywywang <tony_wywang@163.com>
2019-08-12 22:39:29 -0400

    Add pycurl post example

--- library_example/pycurl_exp.py
+++ library_example/pycurl_exp.py
@@ -39,3 +39,26 @@ c.perform()
 c.close()
 
 print(t.contents)
+
+import pycurl
+import certifi
+try:
+    # python 3
+    from urllib.parse import urlencode
+except ImportError:
+    # python 2
+    from urllib import urlencode
+
+c = pycurl.Curl()
+c.setopt(c.URL, 'https://httpbin.org/post')
+
+post_data = {'field': 'value'}
+# Form data must be provided already urlencoded.
+postfields = urlencode(post_data)
+# Sets request method to POST,
+# Content-Type header to application/x-www-form-urlencoded
+# and data to send in request body.
+c.setopt(c.POSTFIELDS, postfields)
+c.setopt(pycurl.CAINFO, certifi.where())
+c.perform()
+c.close()
