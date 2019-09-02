4b283abff3f072e239f850c0a5de9ee7dae8ba67
tonywywang <tony_wywang@163.com>
2019-09-01 23:41:17 -0400

    Add socket gethostbyname gaierror handling example

--- library_example/socket_exp.py
+++ library_example/socket_exp.py
@@ -2,3 +2,15 @@ import socket
 
 ip = socket.gethostbyname('www.google.com')
 print(ip)
+
+import socket
+import sys
+
+try:
+    ip = socket.gethostbyname('www.goooooooooooogle.com')
+except socket.gaierror: 
+  
+    # this means could not resolve the host 
+    print("there was an error resolving the host")
+    sys.exit() 
+print(ip)
