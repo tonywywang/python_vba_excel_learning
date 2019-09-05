5e61836c170ca5fcf2289708830b86b25facdac1
tonywywang <tony_wywang@163.com>
2019-09-04 23:25:11 -0400

    Add multi-connector example

--- library_example/socket_exp.py
+++ library_example/socket_exp.py
@@ -46,3 +46,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = s.recv(1024)
 
 print('Received', repr(data))
+
+import selectors
+import socket
+import sys
+
+host = sys.argv[1]
+port = sys.argv[2]
+sel = selectors.DefaultSelector()
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print('listening on', (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
