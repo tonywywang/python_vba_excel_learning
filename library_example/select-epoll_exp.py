b97725470b5e1f505d7ed4a3e517da91afde1f53
tonywywang <tony_wywang@163.com>
2019-09-19 22:57:35 -0400

    Add select epoll example comment to help understand

--- library_example/select-epoll_exp.py
+++ library_example/select-epoll_exp.py
@@ -46,3 +46,28 @@ finally:
     epoll.unregister(serversocket.fileno())
     epoll.close()
     serversocket.close()
+
+'''
+Line 1: The select module contains the epoll functionality.
+Line 13: Since sockets are blocking by default, this is necessary to use non-blocking (asynchronous) mode.
+Line 15: Create an epoll object.
+Line 16: Register interest in read events on the server socket. A read event will occur any time the server socket accepts a socket connection.
+Line 19: The connection dictionary maps file descriptors (integers) to their corresponding network connection objects.
+Line 21: Query the epoll object to find out if any events of interest may have occurred. The parameter "1" signifies that we are willing to wait up to one second for such an event to occur. If any events of interest occurred prior to this query, the query will return immediately with a list of those events.
+Line 22: Events are returned as a sequence of (fileno, event code) tuples. fileno is a synonym for file descriptor and is always an integer.
+Line 23: If a read event occurred on the socket server, then a new socket connection may have been created.
+Line 25: Set new socket to non-blocking mode.
+Line 26: Register interest in read (EPOLLIN) events for the new socket.
+Line 31: If a read event occurred then read new data sent from the client.
+Line 33: Once the complete request has been received, then unregister interest in read events and register interest in write (EPOLLOUT) events. Write events will occur when it is possible to send response data back to the client.
+Line 34: Print the complete request, demonstrating that although communication with clients is interleaved this data can be assembled and processed as a whole message.
+Line 35: If a write event occurred on a client socket, it's able to accept new data to send to the client.
+Lines 36-38: Send the response data a bit at a time until the complete response has been delivered to the operating system for transmission.
+Line 39: Once the complete response has been sent, disable interest in further read or write events.
+Line 40: A socket shutdown is optional if a connection is closed explicitly. This example program uses it in order to cause the client to shutdown first. The shutdown call informs the client socket that no more data should be sent or received and will cause a well-behaved client to close the socket connection from it's end.
+Line 41: The HUP (hang-up) event indicates that the client socket has been disconnected (i.e. closed), so this end is closed as well. There is no need to register interest in HUP events. They are always indicated on sockets that are registered with the epoll object.
+Line 42: Unregister interest in this socket connection.
+Line 43: Close the socket connection.
+Lines 18-45: The try-catch block is included because the example program will most likely be interrupted by a KeyboardInterrupt exception
+Lines 46-48: Open socket connections don't need to be closed since Python will close them when the program terminates. They're included as a matter of good form.
+'''
