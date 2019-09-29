15e4c75549e7fe51872c08e86ef48e99b18ebd27
tonywywang <tony_wywang@163.com>
2019-09-28 23:30:31 -0400

    Add socket connect error exception handling

--- library_example/socket_ask_for_file.py
+++ library_example/socket_ask_for_file.py
@@ -5,7 +5,11 @@ host = sys.argv[1]
 filename = sys.argv[2]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, 80)) # getsockaddrarg: AF_INET address must be tuple, not str
+try:
+    s.connect((host, 80)) # getsockaddrarg: AF_INET address must be tuple, not str
+except socket.gaierror as e:
+	print("Error connecting to server %s"  % e)
+	sys.exit(1)
 
 s.sendall("filename + '\r\n'".encode()) #  a bytes-like object is required, not 'str'
 while True:
