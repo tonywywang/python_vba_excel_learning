276958f731cf85d83dd30529bd7dbf97f3a4767a
tonywywang <tony_wywang@163.com>
2019-10-05 20:52:10 -0400

    Add socket send recv error handling

--- library_example/socket_test.py
+++ library_example/socket_test.py
@@ -1,11 +1,21 @@
 import socket
+import sys
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8080       # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    try:
+        s.sendall(b'Hello, world')
+    except socket.error as e:
+        print("sendall error: %s" % e)
+        sys.exit(1)
+
+    try:
+        data = s.recv(1024)
+    except socket.error as e:
+    	print("recv error: %s" % e)
+    	sys.exit(1)
 
 print('Received', repr(data))
