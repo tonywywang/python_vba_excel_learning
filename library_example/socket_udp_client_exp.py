c427878b41e3180bd315f44b2096c0858ee0b1c2
tonywywang <tony_wywang@163.com>
2019-10-09 23:30:52 -0400

    Add udp connect and send message to server and wait for response

--- library_example/socket_udp_client_exp.py
+++ library_example/socket_udp_client_exp.py
@@ -9,3 +9,14 @@ try:
     port = int(textport)
 except ValueError:
     port = s.getservbyname(textport, 'udp')
+
+s.connect((host, port))
+print("Enter some message to send:")
+message = sys.stdin.readline().strip()
+s.sendall(message.encode(encoding='utf_8', errors='strict'))
+print("Ctrl + C to terminate")
+while 1:
+	buf = s.recv(2048).decode(encoding='utf_8', errors='strict')
+	if not len(buf):
+		break
+	sys.stdout.write(buf)
